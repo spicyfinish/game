e64e61fb341d75650f377573f2fd3b8bba89a2cc
Gavin Rough <gavin@grough.com>
2022-09-25 15:34:02 -0400

    Mark the viewport's centre

--- Game.gd
+++ Game.gd
@@ -1,5 +1,8 @@
 extends Node
 
+signal paused
+signal resumed
+
 func _ready():
 	$PauseScreen.visible = false
 
@@ -8,9 +11,11 @@ func _input(event):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		get_tree().paused = true
 		$PauseScreen.visible = true
+		emit_signal("paused")
 
 	if event.is_action_pressed("click") and get_tree().paused == true:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		get_tree().paused = false
 		$PauseScreen.visible = false
+		emit_signal("resumed")
 
